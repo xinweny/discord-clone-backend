fece0b9414dbb8d74c49bc0a8916006c04600d40
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-05-04 14:57:51 -0400

    change active to verified

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -4,14 +4,9 @@ const UserSchema = new Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
   email: { type: String, required: true },
-  active: { type: Boolean, default: false },
+  verified: { type: Boolean, default: false },
   joinedAt: { type: Date, required: true },
   avatarUrl: { type: String },
-  servers: [{
-    _id: { type: Schema.Types.ObjectId, ref: 'Server' },
-    name: { type: String, required: true },
-    imgUrl: { type: String },
-  }],
 });
 
 const User = mongoose.model('User', UserSchema);
