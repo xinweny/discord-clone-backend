71033ee1c0c35c8cc07d020f17a26e6a77bb7629
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-05-04 17:06:45 -0400

    add default 500 error

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -4,9 +4,9 @@ const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   res.status(err.statusCode);
 
   res.json({
-    statusCode: err.statusCode,
-    error: err.error,
-    message: err.message,
+    statusCode: err.statusCode || 500,
+    error: err.error || {},
+    message: err.message || 'Internal Server Error',
   });
 };
 
