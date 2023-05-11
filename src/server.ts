3c8fb927d5360233c1ac72acffd8d38fa96167c0
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-05-11 15:18:24 -0400

    rename socket.io config

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import { createServer } from 'http';
 import { Server } from 'socket.io';
-import addListeners from './listeners/index';
+
+import socketIo from './listeners/index';
 
 import app from './app';
 
 const server = createServer(app);
-const io = new Server(server);
+export const io = new Server(server);
 
-addListeners(io);
+socketIo(io);
 
 server.on('error', () => console.log('Error opening server'));
 
